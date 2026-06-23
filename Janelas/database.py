77d7ecd0e6d8df70bfb5f1b284a2ccf40ec6eed5
unknown <epmedeiros@inf.ufpel.edu.br>
2026-06-22 22:08:08 -0300

    correção da conexão com BD

--- Janelas/database.py
+++ Janelas/database.py
@@ -1,9 +1,13 @@
 import psycopg2
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 def conectar():
     return psycopg2.connect(
-        host="localhost",
-        database="PET",
-        user="postgres",
-        password="123123"
+        host=os.getenv("DB_HOST"),
+        database=os.getenv("DB_NAME"),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD")
     )
